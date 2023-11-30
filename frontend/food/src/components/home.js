ea4088b01790eabadff05b3b532c80a8bbaa9c82
Anselmo5 <anselmohenrique191@gmail.com>
2023-11-30 15:51:44 -0300

    update

--- frontend/food/src/components/home.js
+++ frontend/food/src/components/home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './home.css'
 import axios from 'axios';
 import footerexport from '../assets/Group 5946.png'
+import lupa from '../assets/lupa.png'
 import logo from '../assets/Polygon 1.png'
 import logo2 from '../assets/pngegg 1.png'
 import { Link } from 'react-router-dom'
@@ -14,7 +15,16 @@ import saida from '../assets/sair.png'
 const Home = () => {
   return (
     <div>
-       
+       <nav>
+          <div className='alingcontainer'>
+              <img src={footerexport}/>
+              <div className='inpicon'>
+                <button>  <img src={lupa}/></button>
+                <input className='inp' placeholder='Busque por pratos ou ingredientes'></input>
+              </div>
+              <button>Pedidos(0)</button>
+          </div>
+       </nav>
 
     </div>
   )
